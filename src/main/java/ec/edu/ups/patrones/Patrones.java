2fe20c6554d30a3615ce06fa1c2fff85d5cb94e5
ESTUDIANTE <ESTUDIANTE@172.16.214.75>
2023-04-10 10:37:59 -0500

    Patrones1

--- src/main/java/ec/edu/ups/patrones/Patrones.java
+++ src/main/java/ec/edu/ups/patrones/Patrones.java
@@ -1,10 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.ups.patrones;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
@@ -12,12 +16,27 @@ import java.io.File;
  */
 public class Patrones {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
-        File fichero = new File("C:\\Users\\ESTUDIANTE\\Documents\\NetBeansProjects\\Patrones\\src\\fichero.txt");
-                
+        var ruta = "C:\\Users\\ESTUDIANTE\\Documents\\NetBeansProjects\\Patrones\\src\\escribir.txt";
+        String usuario1 = "Felipe";
+        String usuario2 = "Pancracio";
+   
+        File fichero = null;
+        fichero = new File(ruta);
+        PrintWriter pw = null;
+        pw = new PrintWriter(fichero);
+        
+        
+
+      
+            pw.write("Usuario: " + usuario1);
+            pw.write(" ");
+            pw.write("Usuario: " + usuario2);
+        
+
         System.out.println("");
+
     }
-    
-    
+
 }
